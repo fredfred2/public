dc6982374a96414d1f6ef4c025291e2e31d0c55d
Papu <fredfred2@gmail.com>
2019-08-23 00:07:02 -0500

    Se sube el facerecognition

---
+++ total_webcam.py
@@ -0,0 +1,124 @@
+# Import required modules
+import cv2 as cv
+import math
+import time
+import argparse
+from keras.preprocessing.image import img_to_array
+import imutils
+import cv2
+from keras.models import load_model
+import numpy as np
+
+def getFaceBox(net, frame, conf_threshold=0.7):
+    frameOpencvDnn = frame.copy()
+    frameHeight = frameOpencvDnn.shape[0]
+    frameWidth = frameOpencvDnn.shape[1]
+    blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+    net.setInput(blob)
+    detections = net.forward()
+    bboxes = []
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > conf_threshold:
+            x1 = int(detections[0, 0, i, 3] * frameWidth)
+            y1 = int(detections[0, 0, i, 4] * frameHeight)
+            x2 = int(detections[0, 0, i, 5] * frameWidth)
+            y2 = int(detections[0, 0, i, 6] * frameHeight)
+            bboxes.append([x1, y1, x2, y2])
+            cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+    return frameOpencvDnn, bboxes
+
+
+parser = argparse.ArgumentParser(description='Use this script to run age and gender recognition using OpenCV.')
+parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.')
+PADDING = 20
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+EMOTIONS = ["Enojado" ,"Desagrado","Espantado", "Feliz", "Triste", "Sorprendido", "Neutral"]
+args = parser.parse_args()
+
+faceProto = "opencv_face_detector.pbtxt"
+faceModel = "opencv_face_detector_uint8.pb"
+
+ageProto = "age_deploy.prototxt"
+ageModel = "age_net.caffemodel"
+
+genderProto = "gender_deploy.prototxt"
+genderModel = "gender_net.caffemodel"
+
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+ageList = ['(25-32)', '(25-32)', '(25-32)', '(25-32)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList = ['Hombre', 'Mujer']
+
+# Load network
+ageNet = cv.dnn.readNet(ageModel, ageProto)
+genderNet = cv.dnn.readNet(genderModel, genderProto)
+faceNet = cv.dnn.readNet(faceModel, faceProto)
+
+# parameters for loading data and images
+detection_model_path = 'haarcascade_files/haarcascade_frontalface_default.xml'
+emotion_model_path = 'models/_mini_XCEPTION.102-0.66.hdf5'
+face_detection = cv2.CascadeClassifier(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+# Open a video file or an image file or a camera stream
+camera = cv.VideoCapture(args.input if args.input else 0)
+
+padding = 20
+while cv.waitKey(1) < 0:
+    # Read frame
+    t = time.time()
+    hasFrame, frame = camera.read()
+    if not hasFrame:
+        cv.waitKey()
+        break
+
+    frameFace, bboxes = getFaceBox(faceNet, frame)
+    if not bboxes:
+        #print("No face Detected, Checking next frame")
+        continue
+
+    for bbox in bboxes:
+        # print(bbox)
+        face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+
+        blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds = genderNet.forward()
+        gender = genderList[genderPreds[0].argmax()]
+        # print("Gender Output : {}".format(genderPreds))
+        #print("Gender : {}, conf = {:.3f}".format(gender, genderPreds[0].max()))
+
+        ageNet.setInput(blob)
+        agePreds = ageNet.forward()
+        age = ageList[agePreds[0].argmax()]
+        #print("Age Output : {}".format(agePreds))
+        #print("Age : {}, conf = {:.3f}".format(age, agePreds[0].max()))
+
+        
+        # cv.imwrite("age-gender-out-{}".format(args.input),frameFace)
+    #print("time : {:.3f}".format(time.time() - t))
+    
+    #reading the frame
+    frame = imutils.resize(frame,width=300)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_detection.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+    emotion='Neutral'
+    canvas = np.zeros((250, 300, 3), dtype="uint8")
+    frameClone = frame.copy()
+    if len(faces) > 0:
+        faces = sorted(faces, reverse=True,
+        key=lambda x: (x[2] - x[0]) * (x[3] - x[1]))[0]
+        (fX, fY, fW, fH) = faces
+                    # Extract the ROI of the face from the grayscale image, resize it to a fixed 28x28 pixels, and then prepare
+            # the ROI for classification via the CNN
+        roi = gray[fY:fY + fH, fX:fX + fW]
+        roi = cv2.resize(roi, (64, 64))
+        roi = roi.astype("float") / 255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+        preds = emotion_classifier.predict(roi)[0]
+        emotion_probability = np.max(preds)
+        emotion = EMOTIONS[preds.argmax()]
+    label = "{},{},{}".format(gender, age,emotion)
+    cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+    cv.imshow("Axity Vision - Segmentador Clientes", frameFace)
